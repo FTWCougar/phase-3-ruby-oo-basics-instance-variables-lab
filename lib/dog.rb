04a9f96e883164ebaf9cd6117a4ed89b0657c143
Cameron <Cameron@dansclan.com>
2022-11-23 08:50:33 -0800

    commit tree 814866c42b99613b8c9666468f578cb9117003b6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    attr_accessor(name)
+    # def name=(value)
+    #     @this_dogs_name = value
+    # end
+    # def name 
+    #     @this_dogs_name
+    # end
 
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
